1864a59c2d97afc06697581e919703625845e8ed
Sai Chinmay <47316906+saichinmay2000@users.noreply.github.com>
2023-02-25 09:51:53 +0530

    Added States and Districts

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,13 @@ function Home(){
         }} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Get Time Slot Data API
       </button>
-      
+      <button type="button" onClick={
+        (e) => {
+          e.preventDefault()
+          window.location.href='/api/states-districts'
+        }} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        States & District API
+      </button>
       </div>
     </div>
   )
