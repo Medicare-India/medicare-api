0571ed7d3e34a4bc9e0bb731ad0050c18f1026f2
Sai Chinmay Tripurari <122612890+medicare2023@users.noreply.github.com>
2023-06-01 22:58:35 +0530

    Update get_drug_data.js

--- pages/api/get_drug_data.js
+++ pages/api/get_drug_data.js
@@ -1,18 +1,4 @@
-6// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import NextCors from "nextjs-cors";
-import GetDrugData  from "../../prisma/All_Drugs.js";
 
-export default async function handler(req, res) {
-  await NextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  })
-  if (req.method === "GET") {
-    const { name } = req.query;
-    // const result = await GetDrugData(name);
-    // res.send(result)
-    res.send("Hey!")
-  }
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
